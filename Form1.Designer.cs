9f462bbff210f7a1165694a3674fcde2cc020d03
GamesBro <62087206+GamesBro@users.noreply.github.com>
2020-03-21 11:47:38 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace LabPrakt4zad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.University = new System.Windows.Forms.ComboBox();
+            this.Faculties = new System.Windows.Forms.ComboBox();
+            this.Students = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // University
+            // 
+            this.University.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.University.FormattingEnabled = true;
+            this.University.Location = new System.Drawing.Point(13, 34);
+            this.University.Name = "University";
+            this.University.Size = new System.Drawing.Size(388, 28);
+            this.University.TabIndex = 0;
+            // 
+            // Faculties
+            // 
+            this.Faculties.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Faculties.FormattingEnabled = true;
+            this.Faculties.Location = new System.Drawing.Point(13, 128);
+            this.Faculties.Name = "Faculties";
+            this.Faculties.Size = new System.Drawing.Size(388, 28);
+            this.Faculties.TabIndex = 1;
+            // 
+            // Students
+            // 
+            this.Students.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Students.FormattingEnabled = true;
+            this.Students.Location = new System.Drawing.Point(13, 220);
+            this.Students.Name = "Students";
+            this.Students.Size = new System.Drawing.Size(388, 28);
+            this.Students.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Students);
+            this.Controls.Add(this.Faculties);
+            this.Controls.Add(this.University);
+            this.Name = "Form1";
+            this.Text = "Students";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox University;
+        private System.Windows.Forms.ComboBox Faculties;
+        private System.Windows.Forms.ComboBox Students;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace LabPrakt4zad
+{
+    public partial class Form1 : Form
+    {
+		DataSet alldata = new DataSet();
+		DataTable AddIntoDataSet(NpgsqlCommand comma,string name_table)
+		{
+			DataTable table = new DataTable(name_table);
+			table.Load(comma.ExecuteReader());
+			return table;
+		}
+		void LoadList()
+		{
+			DataTable Univ;
+			DataTable Fac;
+			DataTable Stud;
+			using (var connect = new NpgsqlConnection("host = localhost; username = postgres; password = 22443311q; database = prakt"))
+			{
+				connect.Open();
+				using (var commandUniiverse = new NpgsqlCommand("select * from \"University\";", connect))
+				{
+					Univ = AddIntoDataSet(commandUniiverse,"university");
+					alldata.Tables.Add(Univ);
+				}
+				using (var commandFac = new NpgsqlCommand("select * from \"Faculties\";", connect))
+				{
+					Fac = AddIntoDataSet(commandFac, "faculties");
+					alldata.Tables.Add(Fac);
+				}
+				using (var commandStud = new NpgsqlCommand("select * from \"Students\";", connect))
+				{
+					Stud = AddIntoDataSet(commandStud, "students");
+					DataColumn computedColumn = new DataColumn("studinfo", typeof(string));
+					computedColumn.Expression = "[name_of_st] + ' ' + [surname_of_st] + ' ' +[course] + ' курс' ";
+					Stud.Columns.Add(computedColumn);
+					alldata.Tables.Add(Stud);
+				}
+				connect.Close();
+
+
+			}
+			alldata.Relations.Add(new DataRelation("University_faculties", Univ.Columns["id"], Fac.Columns["id_universe"]));
+			alldata.Relations.Add(new DataRelation("Faculties_students", Fac.Columns["id"], Stud.Columns["id_fac"]));
+
+
+
+		}
+		public Form1()
+        {
+            InitializeComponent();
+			LoadList();
+			University.DataSource = alldata;
+			University.DisplayMember = "university.name";
+			Faculties.ValueMember = "university.id";
+
+
+			Faculties.DataSource = alldata;
+			Faculties.DisplayMember = "university.University_faculties.name_of_fac";
+
+			
+			Students.DataSource = alldata;
+			Students.DisplayMember = "university.University_faculties.Faculties_students.studinfo";
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
